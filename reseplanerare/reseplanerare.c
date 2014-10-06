64da29fa8985f2f126374d948bb0b43690f5de10
Joel Berglund <joel.berglund.5241@student.uu.se>
2014-10-06 13:41:34 +0200

    Fixade med funktionerna för makeMap.

--- reseplanerare/reseplanerare.c
+++ reseplanerare/reseplanerare.c
@@ -13,24 +13,44 @@ struct node{//hållplats
 }
 
 char *getHallplats(char *asdf){ //tar emot en rad från nätverk.txt och returnerar hållplatsen
+  char *fdsa = strchr(asdf, ", ");
+  return memchr(fdsa, ',', strlen(fdsa));
 }
 
 char *getNextHallplats(char *asdf){//tar emot en rad från nätverk.txt och returnerar hållplatsen bussen ankommer till
+  char *fdsa = strchr(strchr(asdf, ", "), ", ");
+  return memchr(fdsa, ',', strlen(fdsa));
 }
 
 char *getBus(char *asdf){//tar emot en rad från nätverk.txt och returnerar bussnumret
+  return memchr(asdf, ',', strlen(asdf));
 }
 
 int getTravelTime(char *asdf){//tar emot en rad från nätverk.txt och returnerar restiden
+  return strchr(strchr(strchr(asdf, ", "), ", "), ", ");
 }
 
-void addTimeTable(node hallplats, char *busNumber, char *slutstationer){//lägger till alla avgångstider för busNumber från hallplats baserat på start.txt
+void addTimeTable(node hallplats, char *busNumber, char *slutstationer_){//lägger till alla avgångstider för busNumber från hallplats baserat på start.txt
+  FILE slutstationer = fopen(slutstationer_);
+  char buffer[128];
+  while(feof(slutstationer)){
+    fgets(buffer, sizeof(buffer), slutstationer);
+    if(strcmp(busNumber, getBus(buffer)) == 0 && strcmp(hallplats->name, getHallplats(buffer)) == 0)
+      addTimeBLÄHÖ(hallplats, buffer);
+  }
 }
 
-void addTimes(node hallplats, node nextHallplast, int travelTime){//lägger till all tider en buss ankommer till/avgår från nextHallplast
+void addTimes(node hallplats, node nextHallplast, int travelTime, char *busNumber){//lägger till all tider en buss ankommer till/avgår från nextHallplast
+  nextHallplats->timeBLÄHÖ = addShitBLÄHÖ(nextHallplats->timeBLÄHÖ, travelTime, busNumber);
+  //metainfo för reverseTimetable
 }
 
 void reverseTimetable(asdf){//går "baklänges" från en slutstation och lägger till alla tider från dess riktning
+  addTimeTable(hallplats, busNumber, slutstationer);
+  while(hallplats->METAINFO){//"föregående" hållplats finns
+    addTimes(hallplats, hallplats->METAINFO, hallplats->METAINFO, busNumber); //"föregående" hållplats / resetid
+    hallplats = hallplats->METAINFO; //"föregående" hållplats
+  }
 }
 
 node makeMap(char *bussar_, char *slutstationer_){
@@ -54,8 +74,8 @@ node makeMap(char *bussar_, char *slutstationer_){
       addTimeTable(hallplats, busNumber, slutstationer_);
     }
     fgets(buffer, sizeof(buffer), bussar);
-    nextHallplats = getHallplats(buffer);
-    if(strcmp(hallplats, nextHallplats) == 0){
+    //nextHallplats = getHallplats(buffer);
+    if(strcmp(nextHallplats, getHallplats(buffer)) == 0){
       addTimes(hallplats, nextHallplats, travelTime);
       if(notInList(nextHallplats, hallplatser)){
 	addToList(nextHallplats, hallplatser);
