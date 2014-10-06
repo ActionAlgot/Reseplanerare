ec65666f57e617361bca6c61980bfaf4baf59a60
Joel Berglund <joel.berglund.5241@student.uu.se>
2014-10-06 15:05:13 +0200

    asdf

--- reseplanerare/reseplanerare.c
+++ reseplanerare/reseplanerare.c
@@ -2,14 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct bNode{//buss
-  int times;//tider
-  node dest;//nästa hållplats
+struct tlNode{
+  int time;
+  tNode tail;
 }
 
-struct node{//hållplats
+struct bNode{//buss
+  char *busNumber;
+  tNode times;//tider
+  hNode dest;//nästa hållplats
+}
+
+struct blNode{
+  bNode buss;
+  blNode tail;
+}
+
+struct hNode{//hållplats
   char *name;
   list bussar;
+}
+
+struct hlNode{
+  hNode hallplats;
+  hlNode tail;
+}
+
+int timeToInt(char *time){
+  if(strlen(time) == 5)
+    return ((time[0]-48)*10+time[1]-48)*60+((time[3]-48)*10+time[4]-48);
+  return (time[0]-48)*60+((time[2]-48)*10+time[3]-48);
 }
 
 char *getHallplats(char *asdf){ //tar emot en rad från nätverk.txt och returnerar hållplatsen
@@ -30,18 +52,34 @@ int getTravelTime(char *asdf){//tar emot en rad från nätverk.txt och returnerar 
   return strchr(strchr(strchr(asdf, ", "), ", "), ", ");
 }
 
-void addTimeTable(node hallplats, char *busNumber, char *slutstationer_){//lägger till alla avgångstider för busNumber från hallplats baserat på start.txt
-  FILE slutstationer = fopen(slutstationer_);
-  char buffer[128];
-  while(feof(slutstationer)){
-    fgets(buffer, sizeof(buffer), slutstationer);
-    if(strcmp(busNumber, getBus(buffer)) == 0 && strcmp(hallplats->name, getHallplats(buffer)) == 0)
-      addTimeBLÄHÖ(hallplats, buffer);
-  }
+int getTime(char *asdf){
+  return timeToInt(strchr(strchr(asdf, ", "), ", "));
 }
 
-void addTimes(node hallplats, node nextHallplast, int travelTime, char *busNumber){//lägger till all tider en buss ankommer till/avgår från nextHallplast
-  nextHallplats->timeBLÄHÖ = addShitBLÄHÖ(nextHallplats->timeBLÄHÖ, travelTime, busNumber);
+void addBusStart(node hallplats, node nextHallplats, char *busNumber, char *slutstationer_){//lägger till alla avgångstider för busNumber från hallplats baserat på start.txt
+  FILE slutstationer = fopen(slutstationer_);
+  char buffer[128];
+  bNode buss = malloc(sizeof(bNode));
+  list timetable;
+  strcpy(buss->busNumber, busNumber);
+  buss->dest = nextHallplats;
+  while(feof(slutstationer)){
+    fgets(buffer, sizeof(buffer), slutstationer);
+    if(strcmp(busNumber, getBus(buffer)) == 0 && strcmp(hallplats->name, getHallplats(buffer)) == 0){
+      addToList(timetable, getTime(buffer));
+    }
+  }
+  buss->times = timetable;
+  addToList(hallplats->bussar, buss);
+}
+
+void addBus(node prevHallplats, node hallplats, int travelTime, char *busNumber){//lägger till all tider en buss ankommer till/avgår från nextHallplast
+  bNode buss = malloc(sizeof(bNode));
+  list timetable;
+  list prevTimes = findBus(prevHallplats->bussar, busNumber)->times;
+  while(times != NULL){
+    
+  }
   //metainfo för reverseTimetable
 }
 
@@ -71,7 +109,7 @@ node makeMap(char *bussar_, char *slutstationer_){
     travelTime = getTravelTime(buffer);
     if(notInlist(hallplats, hallplatser)){
       addToList(hallplats, hallplatser);
-      addTimeTable(hallplats, busNumber, slutstationer_);
+      addTimeTable(hallplats, nextHallplats, busNumber, slutstationer_);
     }
     fgets(buffer, sizeof(buffer), bussar);
     //nextHallplats = getHallplats(buffer);
